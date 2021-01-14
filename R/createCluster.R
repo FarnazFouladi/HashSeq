a3c968db27d7998e7ad81395bf696724a646e709
Farnaz Fouladi <ffouladi@uncc.edu>
2021-01-14 11:50:12 -0500

    created and external folder

--- R/createCluster.R
+++ R/createCluster.R
@@ -20,7 +20,7 @@ createCluster <- function(inputDir,outputDir,finaloutput=FALSE)
                        '-finaloutput',finaloutput)
 
   inputArray <- rJava::.jarray(strsplit(inputParams, ' ')[[1]])
-  jarDir <- '.'
+  jarDir <- system.file('inst',package = 'HashSeq')
   jarName <- 'SequenceVariant.jar'
 
   # Add JAR file to class path
