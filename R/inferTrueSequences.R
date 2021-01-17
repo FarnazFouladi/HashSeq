8d59680b96d45ea4e14d515f3ab63f6db21a75a4
jyoung67 <36745757+jyoung67@users.noreply.github.com>
2021-01-17 11:37:43 -0500

    1. Added new pipeline driver R file (inferTrueSequences.R) 2. Changed visibily of helper R functions 3. Removed dependency to fansi package

---
+++ R/inferTrueSequences.R
@@ -0,0 +1,42 @@
+#' Infer true amplicon sequence variants
+#'
+#'Invokes algorithm to infer true amplicon sequence variants.
+#'
+#' @param inputDir String
+#' @param outputDir String
+#' @param countTable Boolean
+#'
+#' @return TODO:  determine what result to return
+#' @export
+#'
+#'
+inferTrueSequences <- function(inputDir,outputDir,abundanceThreshold=1000, countTable=FALSE)
+{
+  print("***************PLEASE WAIT.  PROCESSING INPUT SEQUENCES***************")
+  printDim <- function(df, name)
+  {
+    print(paste0("*****Dimensions of ", name, "*****"))
+    print(paste0("number of rows: ", nrow(df)))
+    print(paste0("number of columns: ", ncol(df)))
+    cat('\n')
+  }
+  processCluster(inputDir,outputDir)
+
+  cluster <- loadCluster()
+  printDim(cluster, "Cluster Table")
+
+  dfAbundantParents <- getAbundantParents(cluster,abundanceThreshold)
+  printDim(dfAbundantParents, "Abundance Parents Table")
+
+  childrenProperties <- getMeanAndSD(cluster)
+  printDim(childrenProperties, "Children Properties Table")
+
+  significantChildren <- infer(childrenProperties,cluster,abundanceThreshold)
+  printDim(significantChildren, "Significant Children Table")
+
+  writeFastaSeq(significantChildren,dfAbundantParents)
+
+  print("******INFERENCE PROCESSING IS COMPLETE******")
+  print("TODO:  Add text that informs the user about which methods can be invoked to visualize results.")
+
+}
