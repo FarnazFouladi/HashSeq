4ebae8112d81ac4fae466355df5d91c8fcd46d60
Farnaz Fouladi <ffouladi@uncc.edu>
2021-01-21 21:09:05 -0500

    read.table replaced with loadOneMismatchCluster

--- R/loadCluster.R
+++ R/loadCluster.R
@@ -12,7 +12,7 @@ loadCluster<-function(outputDir){
   addLeadingZeros <- function(input, maxCount){paste0(parentPrefix, paste0(rep("0", maxCount-nchar(strsplit(input, parentPrefix)[[1]][2])), collapse=""), strsplit(input, "parent")[[1]][2]) }
 
   # Retrieve source data file
-  df <- read.table(file.path(outputDir,"OneMismatchCluster.txt") ,sep="\t", header=TRUE, stringsAsFactors=F)
+  df <- loadOneMismatchClusterFile(outputDir)
   offset <- max(nchar(df$Parent)) - numOfPrefixCharacters
   df$Parent = sapply(df$Parent, addLeadingZeros, numOfPrefixCharacters)
   df<-df %>% mutate(Parent.child=paste0(.data$Parent, "_", .data$Child))
