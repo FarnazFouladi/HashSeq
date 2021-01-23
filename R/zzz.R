453ca4e8cce563b3fb943c5c3316cfc1c95a81e9
Farnaz Fouladi <ffouladi@uncc.edu>
2021-01-22 22:42:12 -0500

    Remove addional parameters

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,3 @@
 .onLoad <- function(libname, pkgname) {
   rJava::.jpackage(pkgname, lib.loc=libname)
-  op <- options()
-  op.devtools <- list(
-    devtools.path = "~/R-dev",
-    devtools.install.args = "",
-    devtools.name = "Farnaz Fouladi; Jack Young",
-    devtools.desc.author = "Farnaz Fouldi [aut, cre]; Jack Young [aut, cre]",
-    devtools.desc.license = "MIT",
-    devtools.desc.suggests = NULL,
-    devtools.desc = list()
-  )
-  toset <- !(names(op.devtools) %in% names(op))
-  if(any(toset)) options(op.devtools[toset])
-
-  invisible()
 }
